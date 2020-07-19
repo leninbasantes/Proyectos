e994b7d42cb52db1fc0cff580bf867137dbb9bbc
Lenin Basantes <leninbasantes@hotmail.com>
2020-07-19 18:44:41 -0500

    Cambio valido

--- Deber_19_casa/Ejercicio_1/index.php
+++ Deber_19_casa/Ejercicio_1/index.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
     <html>
     <head>
-    <script type="text/javascript" src="jquery-1.7.1.min.js">
+    <script type="text/javascript" src="jquery-1.7.1.min.js">//Aqui se procede a utilizar el "jquesy" que descargue 
     </script>
 
     </head>
     <body>
-    <script type="text/javascript">
-    $.getJSON("ajax.json",function(data)){
-        for(usuario in data.users){
-            $("#usuarios".append("<option>"+data.users[usuario].nombre+"</option>"));
-        }
-    };
-    </script>
-    </body>
-    </html>>
+	<p>Archivos JSON</p>
+	<select id="usuarios"></select>	
+</body>
+	
+<script type = "text/javascript" language = "javascript">
+	$.getJSON('ajax.json', function(data) { 
+		for(i in data.users){
+			console.log(data.users[i].nombre)
+			$('#usuarios').append("<option>"+data.users[i].nombre+"</option>")
+		}
+    });
+</script>
+</html>
